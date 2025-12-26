0aa19da61111ecc78bba2807446fde4a3d7358c9
Nurbol Nygmanov <nurbol7@gmail.com>
2025-12-26 09:04:00 +0100

    bundle index

---
+++ .github/actions/bundle.js
@@ -0,0 +1,18 @@
+import core from "@actions/core";
+import github from "@actions/github";
+try {
+    core.debug("Debugging is enabled");
+    core.error("This is an error message");
+    core.warning("This is a warning message");
+    var name = core.getInput("who-to-greet");
+    core.info("Hello ".concat(name, "!"));
+    var time = new Date().toTimeString();
+    core.setOutput("time", time);
+    core.exportVariable("HELLO_TIME", time);
+    core.startGroup("Logging GitHub context");
+    core.info(JSON.stringify(github.context, null, 2));
+    core.endGroup();
+} catch (error) {
+    core.setFailed(error.message);
+}
+
